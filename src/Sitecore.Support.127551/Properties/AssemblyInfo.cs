2aa475e4a8bb5bf556805d7ee2025df5c20c0451
DK\obr <obr@sitecore.net>
2017-06-20 17:32:06 +0300

    commit

---
+++ src/Sitecore.Support.127551/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.127551")]
+[assembly: AssemblyProduct("Sitecore.Support.127551")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.127551/QueryBuilder.cs
@@ -0,0 +1,49 @@
+﻿using Sitecore.ContentSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support
+{
+    class QueryBuilder : Sitecore.ContentSearch.Utilities.QueryBuilder
+    {
+        protected override Type ResolveFieldTypeByName(string fieldName, IProviderSearchContext context)
+        {
+            string str;
+            if (context == null)
+            {
+                return null;
+            }
+            if (context.Index == null)
+            {
+                return null;
+            }
+            if (context.Index.Configuration == null)
+            {
+                return null;
+            }
+            IFieldMap fieldMap = context.Index.Configuration.FieldMap;
+            if (fieldMap == null)
+            {
+                return null;
+            }
+            AbstractSearchFieldConfiguration fieldConfiguration = fieldMap.GetFieldConfiguration(fieldName);
+            if (fieldConfiguration == null)
+            {
+                return null;
+            }
+            if (!fieldConfiguration.Attributes.TryGetValue("type", out str))
+            {
+                return null;
+            }
+            if (string.IsNullOrEmpty(str))
+            {
+                return null;
+            }
+            return Type.GetType(str, false);
+        }
+
+    }
+}
